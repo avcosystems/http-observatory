becca61fbdcda2c4b96c7a2a32fecb8aa8c6fb6a
Lachlan Munro <lachlan.munro@avcosystems.com>
2017-06-23 12:47:42 +0100

    Add port args to python shim

--- scan-url.py
+++ scan-url.py
@@ -1,4 +1,14 @@
 from httpobs.scanner.local import scan
 import sys
+import pprint
 
-print(scan(sys.argv[1]))
+if len(sys.argv) == 0:
+	pprint.pprint("You must supply an endpoint argument")
+	sys.exit()
+
+if len(sys.argv) == 2:
+	pprint.pprint("Scanning: '" + sys.argv[1] + "'")
+	pprint.pprint(scan(sys.argv[1]))
+else:
+	pprint.pprint("Scanning: '" + sys.argv[1] + "' http_port: '" + sys.argv[2] + "' https_port: '" + sys.argv[3] + "'")
+	pprint.pprint(scan(sys.argv[1], https_port=sys.argv[2]))
