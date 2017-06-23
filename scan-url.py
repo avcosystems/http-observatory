0e89762e6c0f750892dd500dc410780d083a189e
Lachlan Munro <lachlan.munro@avcosystems.com>
2017-06-23 14:19:28 +0100

    Better print formatting

--- scan-url.py
+++ scan-url.py
@@ -1,14 +1,14 @@
 from httpobs.scanner.local import scan
 import sys
-import pprint
+import json
 
 if len(sys.argv) == 0:
-	pprint.pprint("You must supply an endpoint argument")
-	sys.exit()
+	print("You must supply an endpoint argument")
+	sys.exit(1)
 
 if len(sys.argv) == 2:
-	pprint.pprint("Scanning: '" + sys.argv[1] + "'")
-	pprint.pprint(scan(sys.argv[1]))
+	print("Scanning: '" + sys.argv[1] + "'")
+	print(json.dumps(scan(sys.argv[1]), sort_keys=True, indent=2))
 else:
-	pprint.pprint("Scanning: '" + sys.argv[1] + "' http_port: '" + sys.argv[2] + "' https_port: '" + sys.argv[3] + "'")
-	pprint.pprint(scan(sys.argv[1], https_port=sys.argv[2]))
+	print("Scanning: '" + sys.argv[1] + "' http_port: '" + sys.argv[2] + "' https_port: '" + sys.argv[3] + "'")
+	print(json.dumps(scan(sys.argv[1], https_port=sys.argv[2]), sort_keys=True, indent=2))
