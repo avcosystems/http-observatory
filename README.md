2c6f53da843cdfbd14623d86cc71b7502bcc353d
Lachlan Munro <lachlan.munro@avcosystems.com>
2021-03-04 17:54:27 +0000

    Add some really dumb hacks to expose more flags

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # http-observatory
 Mozzilla HTTP Observatory dockerfile
+
+## usage
+
+* Clone repo
+* Run `docker build . -t obs` from root
+* Run `docker run obs WEBSITE.COM 80 443 /PATH COOKIE_NAME COOKIE_Value`
--- scan-url.py
+++ scan-url.py
@@ -9,6 +9,15 @@ if len(sys.argv) < 2:
 if len(sys.argv) == 2:
 	print("Scanning: '" + sys.argv[1] + "'")
 	print(json.dumps(scan(sys.argv[1]), sort_keys=True, indent=2))
-else:
+elif len(sys.argv) == 3:
+	print("Scanning: '" + sys.argv[1] + "' http_port: '" + sys.argv[2] + "'")
+	print(json.dumps(scan(sys.argv[1], http_port=sys.argv[2]), sort_keys=True, indent=2))
+elif len(sys.argv) == 4:
 	print("Scanning: '" + sys.argv[1] + "' http_port: '" + sys.argv[2] + "' https_port: '" + sys.argv[3] + "'")
-	print(json.dumps(scan(sys.argv[1], https_port=sys.argv[2]), sort_keys=True, indent=2))
+	print(json.dumps(scan(sys.argv[1], http_port=sys.argv[2], https_port=sys.argv[3]), sort_keys=True, indent=2))
+elif len(sys.argv) == 5:
+	print("Scanning: '" + sys.argv[1] + "' http_port: '" + sys.argv[2] + "' https_port: '" + sys.argv[3] + "'" + "' path: '" + sys.argv[4] + "'")
+	print(json.dumps(scan(sys.argv[1], http_port=sys.argv[2], https_port=sys.argv[3], path=sys.argv[4]), sort_keys=True, indent=2))
+else:
+	print("Scanning: '" + sys.argv[1] + "' http_port: '" + sys.argv[2] + "' https_port: '" + sys.argv[3] + "'" + "' path: '" + sys.argv[4] + "' cookiekey: '" + sys.argv[5] + "' cookievalue: '" + sys.argv[6] + "'")
+	print(json.dumps(scan(sys.argv[1], http_port=sys.argv[2], https_port=sys.argv[3], path=sys.argv[4], cookies={sys.argv[5]: sys.argv[6]}), sort_keys=True, indent=2))
