418521d0fa3c254d8785d5cf58a46f6640fb187b
Lachlan Munro <lachlan.munro@avcosystems.com>
2021-03-04 19:33:22 +0000

    Add another hacky intermediate option for development

--- scan-url.py
+++ scan-url.py
@@ -18,6 +18,9 @@ elif len(sys.argv) == 4:
 elif len(sys.argv) == 5:
 	print("Scanning: '" + sys.argv[1] + "' http_port: '" + sys.argv[2] + "' https_port: '" + sys.argv[3] + "'" + "' path: '" + sys.argv[4] + "'")
 	print(json.dumps(scan(sys.argv[1], http_port=sys.argv[2], https_port=sys.argv[3], path=sys.argv[4]), sort_keys=True, indent=2))
+elif len(sys.argv) == 6:
+	print("Scanning: '" + sys.argv[1] + "' https_port: '" + sys.argv[2] + "'" + "' path: '" + sys.argv[3] + "' cookiekey: '" + sys.argv[4] + "' cookievalue: '" + sys.argv[5] + "'")
+	print(json.dumps(scan(sys.argv[1], https_port=sys.argv[2], path=sys.argv[3], cookies={sys.argv[4]: sys.argv[5]}), sort_keys=True, indent=2))
 else:
 	print("Scanning: '" + sys.argv[1] + "' http_port: '" + sys.argv[2] + "' https_port: '" + sys.argv[3] + "'" + "' path: '" + sys.argv[4] + "' cookiekey: '" + sys.argv[5] + "' cookievalue: '" + sys.argv[6] + "'")
 	print(json.dumps(scan(sys.argv[1], http_port=sys.argv[2], https_port=sys.argv[3], path=sys.argv[4], cookies={sys.argv[5]: sys.argv[6]}), sort_keys=True, indent=2))
